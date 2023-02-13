b4a36f50f0e1a183d9f71e4e36de3465906e28a6
Jorge Enrique Lopez <31283484+jorenlop@users.noreply.github.com>
2023-02-13 17:51:52 -0500

    Create index.php
    archivo php que muestra la fecha

---
+++ html/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Cuantic - Fecha actual</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+      }
+
+      h1 {
+        font-size: 36px;
+        margin-top: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Fecha actual by Jorge Lopez</h1>
+    <p>
+      <?php
+        echo date("d/m/Y");
+      ?>
+    </p>
+  </body>
+</html>
